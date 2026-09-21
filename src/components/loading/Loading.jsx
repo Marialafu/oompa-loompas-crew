8af7a33cb21159eb262b39bc51678041beebcf1d
Marialafu <lafuentelhmaria@gmail.com>
2026-09-21 21:33:52 +0200

    loading

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,13 @@
+import "./Loading.css";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <span className="body loading-content">
+        Cargando datos de la fábrica...
+      </span>
+    </div>
+  );
+};
+
+export default Loading;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import { useEffect, useRef, useState } from "react";
 import { addItems, setItems } from "../../store/oompaSlice";
 import Header from "../../components/header/Header";
 import OompaCard from "../../components/oompaCard/OompaCard";
+import Loading from "../../components/loading/Loading";
 
 const Home = () => {
   const oompas = useSelector((state) => state.oompas.items);
@@ -12,13 +13,14 @@ const Home = () => {
 
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(null);
+  const [isLoading, setLoading] = useState(false);
   const loadRef = useRef(null);
 
   useEffect(() => {
     const fetchOompaCrew = async () => {
       const crew = await getOompaCrew(1);
       dispatch(setItems(crew.results));
-
+      setPage(crew.current);
       setTotalPages(crew.total);
     };
     fetchOompaCrew();
@@ -35,16 +37,22 @@ const Home = () => {
     return () => {
       loadObserver.disconnect();
     };
-  }, [page, totalPages]);
+  }, [page, totalPages, isLoading]);
 
   const loadNextPage = async () => {
-    if (page >= totalPages) return;
+    if (isLoading || page >= totalPages) return;
+    setLoading(true);
 
-    const nextPage = page + 1;
-    const newCrew = await getOompaCrew(nextPage);
-    dispatch(addItems(newCrew.results));
+    try {
+      const nextPage = page + 1;
+      const newCrew = await getOompaCrew(nextPage);
+      console.log(newCrew);
 
-    setPage(newCrew.current);
+      dispatch(addItems(newCrew.results));
+      setPage(newCrew.current);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -56,12 +64,16 @@ const Home = () => {
           <h2 className="subtitle">There are more than 100k</h2>
         </section>
 
-        <div className="oompas-grid">
-          {oompas.map((oompa) => (
-            <OompaCard key={oompa.id} {...oompa} />
-          ))}
+        <div className="oompas-crew">
+          <div className="oompas-grid">
+            {oompas.map((oompa) => (
+              <OompaCard key={oompa.id} {...oompa} />
+            ))}
+          </div>
+
+          {isLoading && <Loading />}
+          <div ref={loadRef} />
         </div>
-        <div ref={loadRef} />
       </main>
     </>
   );
