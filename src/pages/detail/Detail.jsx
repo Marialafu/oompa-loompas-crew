a2f744535c89e32196ea067a6d9ece90ae83489f
Marialafu <lafuentelhmaria@gmail.com>
2026-09-22 21:42:45 +0200

    detail-loading

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,11 +1,12 @@
 import "./Detail.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getOompaById } from "../../api/oompaApi";
 import { isRequestExpired } from "../../utils/isRequestExpired";
 import { setDetails } from "../../store/oompaSlice";
 import Header from "../../components/header/Header";
+import Loading from "../../components/loading/Loading";
 
 const Detail = () => {
   const { id } = useParams();
@@ -15,14 +16,21 @@ const Detail = () => {
   const detail = useSelector((state) => state.oompas.details[id]);
   const oompa = detail?.data;
 
-  const requestExpired = isRequestExpired(detail?.lastRequest);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    const requestExpired = isRequestExpired(detail?.lastRequest);
     if (!requestExpired) return;
 
     const fetchOompaDetail = async () => {
-      const oompa = await getOompaById(id);
-      dispatch(setDetails({ id, oompa }));
+      setIsLoading(true);
+
+      try {
+        const oompa = await getOompaById(id);
+        dispatch(setDetails({ id, oompa }));
+      } finally {
+        setIsLoading(false);
+      }
     };
     fetchOompaDetail();
   }, [id]);
@@ -31,22 +39,30 @@ const Detail = () => {
     <>
       <Header />
       <main className="detail-main">
-        <div>
-          <img src={oompa?.image} alt="nombre" />
-        </div>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            <div>
+              <img src={oompa?.image} alt="nombre" />
+            </div>
 
-        <div className="detail-content">
-          <div className="detail-info">
-            <h3 className="heading oompa-card-hover">{oompa?.first_name}</h3>
-            <span className="caption">{oompa?.gender}</span>
-            <span className="caption">{oompa?.profession}</span>
-          </div>
+            <div className="detail-content">
+              <div className="detail-info">
+                <h3 className="heading oompa-card-hover">
+                  {oompa?.first_name}
+                </h3>
+                <span className="caption">{oompa?.gender}</span>
+                <span className="caption">{oompa?.profession}</span>
+              </div>
 
-          <div
-            className="body detail-description"
-            dangerouslySetInnerHTML={{ __html: oompa?.description }}
-          />
-        </div>
+              <div
+                className="body detail-description"
+                dangerouslySetInnerHTML={{ __html: oompa?.description }}
+              />
+            </div>
+          </>
+        )}
       </main>
     </>
   );
